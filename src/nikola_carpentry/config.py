f5d09acafd0a435f46ab77b89347413566c114c3
Jon Rickman <jrickman@jrickman.net>
2023-10-11 16:52:40 -0400

    meh

--- src/nikola_carpentry/config.py
+++ src/nikola_carpentry/config.py
@@ -1,18 +1,18 @@
 from pathlib import Path
+
+
 config = {
     # database stuff
-    "SQLALCHEMY_DATABASE_URI" : "sqlite:///app.db",
-    "SECRET_KEY" : "password",
-
+    "SQLALCHEMY_DATABASE_URI": "sqlite:///app.db",
+    "SECRET_KEY": "password",
     # file stuff
-    "ROOT" : Path(__file__).parent,
-    "UPLOAD_FOLDER" : "static",
-    "MAX_CONTENT_LENGTH" : 25 * 1000 * 1000,
-
+    "ROOT": Path(__file__).parent,
+    "UPLOAD_FOLDER": "static",
+    "MAX_CONTENT_LENGTH": 25 * 1000 * 1000,
     # Email settup
-    "SMTP_HOST" : "",
-    "SMTP_PORT" : "",
-    "SMTP_SERVICE_USER" : "",
-    "SMTP_SERVICE_PASSWORD" : "",
-    "OUTGOING_EMAIL_ADDRESS" : "",
+    "SMTP_HOST": "",
+    "SMTP_PORT": "",
+    "SMTP_SERVICE_USER": "",
+    "SMTP_SERVICE_PASSWORD": "",
+    "OUTGOING_EMAIL_ADDRESS": "",
 }
